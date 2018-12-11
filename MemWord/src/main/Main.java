7512cf7e63a860c54cba63dbacec04271245d941
MariAli <humenbeing1337@gmail.com>
2018-12-11 21:44:15 +0900

    Fixed possible encoding issue

--- MemWord/src/main/Main.java
+++ MemWord/src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
 import dbmanager.DB;
 import gui.DictSelectFrame;
 
@@ -13,6 +16,26 @@ public class Main {
 		DB db = new DB();
 		db.import_csv("./test.csv", "test");
 		*/
+		
+		System.setProperty("file.encoding", "UTF-8");
+		Field charset;
+		try {
+			charset = Charset.class.getDeclaredField("defaultCharset");
+			charset.setAccessible(true);
+			charset.set(null, null);
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		DictSelectFrame dsf = new DictSelectFrame();
 		dsf.setVisible(true);
